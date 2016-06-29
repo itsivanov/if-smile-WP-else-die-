cc8131c96ec54867ed849a109e5563094166e0c7
Ivanov Oleg <0681814888@mail.ua>
2016-06-29 16:28:09 +0300

    Delete закрыть блок нажав мыщью не на нем.js

--- закрыть блок нажав мыщью не на нем.js
+++
@@ -1,7 +0,0 @@
-//Close form when you click outside of the field element 
-$(document).mouseup(function (e) {
-   var container = $("form");
-   if (container.has(e.target).length === 0){
-       container.hide();
-   }
-});
